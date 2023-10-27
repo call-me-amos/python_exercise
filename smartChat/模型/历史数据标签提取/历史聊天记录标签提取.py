d9e04c16a101604502ab5b4b4d152ce86e024742
amos.tong@corp.to8to.com <amos.tong@corp.to8to.com>
2023-10-27 11:04:25 +0800

    xx

--- smartChat/模型/历史数据标签提取/历史聊天记录标签提取.py
+++ smartChat/模型/历史数据标签提取/历史聊天记录标签提取.py
@@ -4,6 +4,8 @@ import uuid
 import operator
 import base64
 import pandas as pd
+import 格式化小程序回复 as miniProgramFormat
+import jsonmerge
 
 '''
     捞取历史聊天记录，走话术推荐逻辑，提取标签
@@ -16,24 +18,33 @@ def load_file(file_id):
     return data
 
 
-def test_bot(user_name, text):
-    url = "http://10.4.42.48:40121/tls/smartChat/testReply"
+def user_reply(user_name, text, direction):
+    url = "http://10.4.42.48:40121/tls/smartChat/historyRecordReply"
     mode = 1
-    if text == "":
-        chat_id = base64.encodebytes(("tongzhiwei#" + str(user_name)).encode("utf-8"))
-        print(f"开启会话: user:{user_name}, chatId={str(chat_id, 'utf-8')}")
-        mode = 2
     dataJson = {
         "user": user_name,
         "weChat": "tongzhiwei",  # 设置特殊的顾问id，方便提取数据
         "eventType": mode,
         "messageId": uuid.uuid4().hex,
+        "direction": direction,
         "reply": text
-
     }
     test_bot_res = requests.post(url=url, json=dataJson)
     print(f"test_bot_res={test_bot_res.json()}")
     return test_bot_res.json()
+
+
+def create_session(user_name):
+    url = "http://10.4.42.48:40121/tls/smartChat/historyRecordCreate"
+    chat_id = base64.encodebytes(("tongzhiwei#" + str(user_name)).encode("utf-8"))
+    print(f"开启会话: user:{user_name}, chatId={str(chat_id, 'utf-8')}")
+    dataJson = {
+        "user": user_name,
+        "weChat": "tongzhiwei"
+    }
+    create_session_res = requests.post(url=url, json=dataJson)
+    print(f"test_bot_res={create_session_res.json()}")
+    return create_session_res.json()
 
 
 def get_label_obj(text):
@@ -53,20 +64,35 @@ def update_ask_slot(user_name, intention):
     url = "http://10.4.42.48:40121/tls/smartChat/testForHistoryUpdateAskSlot"
     dataJson = {
         "user": user_name,
-        "weChat": "tongzhiwei",  # 设置特殊的顾问id，方便提取数据
+        "weChat": "tongzhiwei",
         "intention": intention
     }
     update_ask_slot_res = requests.post(url=url, json=dataJson)
     return update_ask_slot_res.json()
 
 
+def historyRecordMiniProgram(user_name, message={}):
+    url = "http://10.4.42.48:40121/tls/smartChat/historyRecordMiniProgram"
+    dataJson = {
+        "user": user_name,
+        "weChat": "tongzhiwei"
+    }
+    dataJson = jsonmerge.merge(dataJson, message)
+    print(f"小程序卡请求参数：{dataJson}")
+    historyRecordMiniProgram_res = requests.post(url=url, json=dataJson)
+    return historyRecordMiniProgram_res.json()
+
+
 def process_reply(row_array):
     print(f"process_reply={str(row_array)}")
     row_phone_id = str(row_array[2])
     row_direction = str(row_array[3])
+    row_send_time = int(row_array[4])
     row_msg_type = str(row_array[5])
     row_content = str(row_array[6])
 
+    row_direction = "2"
+    row_content = "洪果女士，我看到您完善了信息，浙江杭州市萧山区禹洲泊朗廷轩营销中心89m?的毛坯装修，全半包计划选择半包，您想要的风格是现代简约，2023-11-01装修，2024-10-15可以量房。这些信息没问题的话，我尽快帮您安排1-4家公司免费量房出方案和报价，可以吗？"
     if "" == row_content:
         print("回复空字符串")
         return None
@@ -86,13 +112,16 @@ def process_reply(row_array):
 
     # 开启会话
     if "我通过了你的联系人验证请求，现在我们可以开始聊天了" == row_content or "我已经添加了你，现在我们可以开始聊天了。" == row_content:
-        test_bot(row_phone_id, "")
-    elif "2" == row_direction and operator.contains(row_content, "我看到您完善了信息"):
-        # 顾问回复-小程序卡 TODO
-        print("小程序卡 TODO")
+        create_session(row_phone_id)
+    elif "2" == row_direction and (operator.contains(row_content, "我看到您完善了信息") or operator.contains(row_content, "hello，咱们填写的信息是")):
+        miniProgramFormatRes = miniProgramFormat.get_tag(row_content, row_send_time)
+        print(f"小程序卡结构化结果：{miniProgramFormatRes}")
+        if miniProgramFormatRes is not None:
+            historyRecordMiniProgram_res = historyRecordMiniProgram(row_phone_id, miniProgramFormatRes)
+            print(f"回复小程序卡内容处理完成{historyRecordMiniProgram_res}")
     elif "1" == row_direction:
         # 用户回复。走话术推荐逻辑，完成标签提取
-        test_bot(row_phone_id, row_content)
+        user_reply(row_phone_id, row_content, row_direction)
     else:
         # 顾问回复-提问。通过字符相似度匹配逻辑，判断顾问是否在提问
         label_obj_res = get_label_obj(row_content)
@@ -116,7 +145,7 @@ def process_reply(row_array):
 
 if __name__ == '__main__':
     # 打开文件
-    data_df = pd.read_excel('C:\\Users\\amos.tong\\Desktop\\历史记录-标签提取\\聊天记录+顾问意图训练数据.xls')
+    data_df = pd.read_excel('C:\\Users\\amos.tong\\Desktop\\历史记录-标签提取\\聊天记录+顾问意图训练数据.xls', sheet_name='Sheet2')
 
     for index, row in data_df.iterrows():
         process_reply_res = process_reply(row)
